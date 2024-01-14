b7de2839ff27882711ecfff846e232c88f0ff2ef
Anonymous Soldier <a96111085154015@gmail.com>
2024-01-14 21:30:02 +0330

    get coin chart data from api

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -2,6 +2,7 @@
 import styles from "./Chart.module.css";
 
 function Chart({ chart, setChart }) {
+  console.log(chart);
   return (
     <div className={styles.container}>
       <span className={styles.cross} onClick={() => setChart(null)}>
