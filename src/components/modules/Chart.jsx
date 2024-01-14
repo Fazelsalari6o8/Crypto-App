5c2c0e31aac0dcde3ecd13c86c336d52f5d86a1a
Anonymous Soldier <a96111085154015@gmail.com>
2024-01-14 22:25:07 +0330

    changing the data format for graphing

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,8 +1,17 @@
+// hooks
+import { useState } from "react";
+
+// helper
+import { convertData } from "../../helpers/convertData.js";
+
 // style
 import styles from "./Chart.module.css";
 
 function Chart({ chart, setChart }) {
   console.log(chart);
+  const [type, setType] = useState("total_volumes");
+
+  convertData(chart, type);
   return (
     <div className={styles.container}>
       <span className={styles.cross} onClick={() => setChart(null)}>
