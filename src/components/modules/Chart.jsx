190301f6279ad05e230b8723f0b1def4e0c1ab3e
Anonymous Soldier <a96111085154015@gmail.com>
2024-01-14 23:40:39 +0330

    draw a diagram

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -4,22 +4,56 @@ import { useState } from "react";
 // helper
 import { convertData } from "../../helpers/convertData.js";
 
+// library
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
 // style
 import styles from "./Chart.module.css";
 
 function Chart({ chart, setChart }) {
-  console.log(chart);
-  const [type, setType] = useState("total_volumes");
+  const [type, setType] = useState("prices");
 
-  convertData(chart, type);
   return (
     <div className={styles.container}>
       <span className={styles.cross} onClick={() => setChart(null)}>
         X
       </span>
-      <div className={styles.chart}>coins chart</div>
+      <div className={styles.chart}>
+        <div className={styles.graph}>
+          <ChartComponent data={convertData(chart, type)} type={type} />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Chart;
+
+const ChartComponent = ({ data, type }) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart width={400} height={400} data={data}>
+        <CartesianGrid stroke="#404042" />
+        <YAxis dataKey={type} domain={["auto", "auto"]} />
+        <XAxis dataKey="date" hide />
+        <Line
+          type="monotone"
+          dataKey={type}
+          stroke="#3874ff"
+          strokeWidth="2px"
+        />
+        <Tooltip />
+        <Legend />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
